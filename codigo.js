7ae24efe35861e6751af3326849d9ee8c54f4dbf
Nahuel <nahukpo109@gmail.com>
2024-10-18 12:11:52 -0300

    inicio

---
+++ codigo.js
@@ -0,0 +1,67 @@
+const btn = document.querySelector(".btn");
+// navigator.usb.requestDevice()
+//   .then(devices => {
+//     // Mostrar una lista de dispositivos al usuario
+//     devices.forEach((device, index) => {
+//       console.log(`${index + 1}. ${device.productName}`);
+//     });
+
+//     // Pedir al usuario que seleccione un dispositivo
+//     const selectedIndex = prompt('Selecciona el número del dispositivo:');
+//     const selectedDevice = devices[selectedIndex - 1];
+
+//     // Usar el dispositivo seleccionado
+//     selectedDevice.open()
+//       .then(() => {
+//         // ...
+//       });
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+btn.addEventListener("click", async(e) => {
+    e.preventDefault();	
+    await navigator.usb.requestDevice({ filters: [{ vendorId: 0x10C4, productId: 0xEA60 }] })
+  .then(device => {
+    console.log("Dispositivo conectado:", device);
+   device.open()
+      .then(() => {
+        // Seleccionar configuración e interfaz (ajusta según tu dispositivo)
+        const configuration = device.configurations[0];
+        const interface = configuration.interfaces[0];
+        const endpoint = interface.endpoints[0]; // Suponiendo que el primer endpoint es de entrada
+
+        // Crear una tubería
+        endpoint.transferIn(64) // 64 bytes es un tamaño de transferencia común, ajusta según sea necesario
+          .then(result => {
+            // Los datos leídos están en result.data
+            console.log("Datos recibidos:", result.data);
+
+            // Continuar leyendo datos
+            endpoint.transferIn(64).then(/* ... */);
+          });
+      }).catch(error => {
+        if (error.name === 'SecurityError') {
+          console.error('User denied permission', error);
+        } else {
+          console.error('Error opening device:', error);
+        }
+      });
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+})
+
+
+
+
+
+// navigator.usb.requestDevice({ filters: [{  vendorId: 0x10C4, productId: 0xEA60 }] })
+//   .then(device => {
+//     // Aquí puedes trabajar con el dispositivo seleccionado por el usuario
+//     console.log("Dispositivo conectado:", device);
+//   })
+//   .catch(error => {
+//     console.log("Error al conectar el dispositivo:", error);
+//   });
